1889a12f1f55ce837d80832c174904102a7cf741
ckhiv <71054929+ckhiv@users.noreply.github.com>
2020-10-09 09:09:31 +0800

    完成用户登录页响应式布局及内容

--- iweb_frontend/js/components.js
+++ iweb_frontend/js/components.js
@@ -1,13 +1,10 @@
-/*1.响应式导航条，点击汉堡包按钮时导航菜单从左侧进入；点击关闭按钮时导航隐藏到左侧*/
-// 根据属性选择器查找一个元素
-document.querySelector('[data-toggle="nav"]').onclick = function() {
-	// alert(123)
-	// 将导航条从左侧移出来——给navbar添加active类
-	document.querySelector('[data-target="nav"]')
-		.classList.add('active')
+/*响应式导航条：点击汉堡包按钮时导航菜单从左侧进入 反之点击关闭按钮导航隐藏在左侧*/
+document.querySelector('[data-toggle="nav"]').onclick=function(){
+	document.querySelector('[data-target="nav"]').classList.add('active')
 }
-// 点击关闭按钮时导航隐藏到左侧
-document.querySelector('[data-close="nav"]').onclick = function() {
-	document.querySelector('[data-target="nav"]')
-		.classList.remove('active')
+
+document.querySelector('[data-click="nav"]').onclick=function(){
+	document.querySelector('[data-target="nav"]').classList.remove('active')
 }
+
+
